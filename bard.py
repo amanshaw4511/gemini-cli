111d457cb2420d8e6c1d3d9dcbd03785698de85e
Aman Shaw <amanshaw4511@protonmail.com>
2023-05-21 23:09:52 +0530

    initial code

---
+++ bard.py
@@ -0,0 +1,78 @@
+"""
+Usage:
+    bard.py [--debug] [--interactive]
+    bard.py [--debug] [INPUT...]
+    bard.py (-h | --help)
+
+Options:
+    -h --help        Show this screen
+    --interactive    Interactive chat mode
+    --debug          Enable debugging output
+
+"""
+
+
+from bardapi import Bard
+from os import environ
+from rich.console import Console
+from rich.markdown import Markdown
+from docopt import docopt
+
+
+debug_enabled = False
+
+def run_interactive(bard) :
+    try :
+        while True:
+            user_input = input("\U0001F464 >> ")
+            if user_input == 'exit' :
+                return
+
+            print()
+            print_markdown_answer(bard, user_input)
+            print('-'*100, "\n")
+
+    except KeyboardInterrupt :
+        return
+
+
+def get_answer(bard, user_input) :
+    response = bard.get_answer(args)
+    print_if_debug_enabled("response", response)
+    return response
+
+
+def print_markdown_answer(bard, user_input) :
+    response = bard.get_answer(user_input)
+    print_if_debug_enabled("response", response)
+    console = Console()
+    md = Markdown("\U0001F916 " + response['content'])
+    console.print(md)
+
+
+def print_if_debug_enabled(name, text):
+    if debug_enabled == False :
+        return
+
+    print(name + " :" , text)
+
+
+if __name__ == "__main__" :
+    arguments = docopt(__doc__)
+    print_if_debug_enabled("arguments", arguments)
+
+    interactive_enabled = arguments['--interactive']
+    debug_enabled = arguments['--debug']
+    user_input = " ".join(arguments['INPUT'])
+
+
+
+    token = environ.get('BARD_TOKEN')
+    bard = Bard(token = token)
+
+
+    if interactive_enabled or user_input.strip() == "" :
+        run_interactive(bard)
+    else :
+        print_markdown_answer(bard, user_input)
+
