87a036ede593ed051281fb1d02013f7d1671530c
Aman Shaw <amanshaw4511@protonmail.com>
2024-03-03 17:43:04 +0530

    add chat feature

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from os import environ
 from sys import argv
 from rich.console import Console
 from rich.markdown import Markdown
+from functools import cache
+import click
 
 import google.generativeai as genai
 
@@ -16,22 +18,51 @@ def mark_print(text_stream):
         text = text.replace('•', '  *')
         markdown = Markdown(text)
         console.print(markdown)
-        # print(chunk)
 
 
-# def to_markdown(text):
-#   text = text.replace('•', '  *')
-#   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+def to_markdown(text):
+    text = text.replace('•', '  *')
+    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
+
+@cache
 def get_model():
     api_key = environ['GEMINI_API_KEY']
     genai.configure(api_key=api_key)
     return genai.GenerativeModel('gemini-pro')
 
 
-if __name__ == '__main__':
+@cache
+def get_chat():
     model = get_model()
-    prompt = ' '.join(argv[1:])
-    print(prompt)
-    resp = model.generate_content(prompt, stream=True)
+    return model.start_chat(history=[])
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command
+def chat():
+    while True:
+        print("> ", end='')
+        inp = input()
+        if inp.strip() == '':
+            continue
+        if inp.strip() == 'quit' or inp.strip() == 'exit':
+            return
+        resp = get_chat().send_message(inp, stream=True)
+        mark_print(resp)
+
+
+@cli.command(name='tell')
+@click.argument('prompt', nargs=-1)
+def query(prompt):
+    prompt = ' '.join(list(prompt))
+    resp = get_model().generate_content(prompt, stream=True)
     mark_print(resp)
+
+
+if __name__ == '__main__':
+    cli()
